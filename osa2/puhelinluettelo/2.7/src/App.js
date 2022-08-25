bb99c426e575c18dc093ef747950803678741a00
T. L <43107946+mpiuv@users.noreply.github.com>
2022-08-25 14:04:33 +0300

    Tehtävä 2.7

---
+++ osa2/puhelinluettelo/2.7/src/App.js
@@ -0,0 +1,57 @@
+import { useState } from 'react'
+
+const Person = (props) =>{
+  return (<li>{props.person}</li>)
+}
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const addName = (event) => {
+    event.preventDefault()
+    const nameObject = {
+      name: newName,
+    }
+
+   const same = (element) => element.name===newName
+
+    if(persons.some(same)) {
+      alert(`${newName} is already added to phonebook`)
+      }
+    else {
+      setPersons(persons.concat(nameObject))
+    }
+    setNewName('')
+  }
+  
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+    
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input value={newName}
+                 onChange={handleNameChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+         {persons.map(person => <Person key={person.name} person={person.name}/>)}
+      </ul>
+
+    </div>
+  )
+}
+
+export default App
