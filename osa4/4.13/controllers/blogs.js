97e83ff44dac4bae140b09da5ff78db25acaa20d
T. L <43107946+mpiuv@users.noreply.github.com>
2022-11-02 12:43:03 +0200

    4.13

---
+++ osa4/4.13/controllers/blogs.js
@@ -0,0 +1,40 @@
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+
+blogsRouter.get('/', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
+
+blogsRouter.post('/', (request, response) => {
+  console.log(request.body)
+  let req=request.body
+  if(request.body.likes===undefined)
+    req={title: request.body.title, author: request.body.author, url:request.body.url, likes:0}
+  if( request.body.title===undefined || request.body.url===undefined){
+    response.status(400).end()
+    return
+  }
+    
+  const blog = new Blog(req)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+})
+
+blogsRouter.delete('/:id', async (request, response, next) => {
+  try {
+    await Blog.findByIdAndRemove(request.params.id)
+    response.status(204).end()
+  } catch (exception) {
+    next(exception)
+  }
+})
+
+module.exports = blogsRouter
