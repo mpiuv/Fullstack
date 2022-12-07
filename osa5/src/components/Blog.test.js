88b3a3ca00d236cf733d21775103f1c64844782c
T. L <43107946+mpiuv@users.noreply.github.com>
2022-12-07 14:33:03 +0200

    5.16

--- osa5/src/components/Blog.test.js
+++ osa5/src/components/Blog.test.js
@@ -3,6 +3,7 @@ import '@testing-library/jest-dom/extend-expect'
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
+import BlogForm from './BlogForm'
 
 test('renders content', () => {
   const blog = {
@@ -64,4 +65,30 @@ test('The like-button is pressed twice and its eventhandler is called twice', as
   await user.click(button)
   await user.click(button)
   expect(mockHandler2.mock.calls).toHaveLength(2)
+})
+
+test('When a blog is created, it is checked if the callback function for creation has been called with right arguments',async() => {
+  const blog = {
+    title: 'Blog title',
+    author: 'A. Author',
+    url: 'http://localhost'
+  }
+
+  const mockHandler = (blogObject) => {
+    expect(blogObject.title).toBe('Blog title')
+    expect(blogObject.author).toBe('A. Author')
+    expect(blogObject.url).toBe('http://localhost')
+  }
+
+  render(<BlogForm  createBlog={mockHandler} />)
+  let user = userEvent.setup()
+
+  const sendButton = screen.getByText('create')
+
+  const inputs = screen.getAllByRole('textbox')
+  await user.type(inputs[0], blog.title)
+  await user.type(inputs[1], blog.author)
+  await user.type(inputs[2], blog.url)
+
+  await user.click(sendButton)
 })
