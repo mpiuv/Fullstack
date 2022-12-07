d262caedbc59158ff620644d206ce57e12f5f940
T. L <43107946+mpiuv@users.noreply.github.com>
2022-12-07 13:01:08 +0200

    5.14

--- osa5/src/components/Blog.test.js
+++ osa5/src/components/Blog.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 test('renders content', () => {
@@ -13,4 +14,32 @@ test('renders content', () => {
 
   const element = screen.getByText('Blog title', { exact: false })
   expect(element).toBeDefined()
+})
+
+test('shows the url, the number of likes, user when the view button has been pressed', async() => {
+  const blog = {
+    title: 'Blog title',
+    author: 'A. Author',
+    likes: 1,
+    url: 'http://localhost',
+    user:{ name:'Evil',username:'root' }
+  }
+
+  render(<Blog blog={blog} username={'root'} removeBlog={jest.fn()}/>)
+  const user = userEvent.setup()
+  const button = screen.getByText('view')
+  await user.click(button)
+
+  let element = screen.getByText('Blog title', { exact: false })
+  expect(element).toBeDefined()
+  element = screen.getByText('A. Author', { exact: false })
+  expect(element).toBeDefined()
+  element = screen.getByText('likes 1', { exact: false })
+  expect(element).toBeDefined()
+  element = screen.getByText('http://localhost', { exact: false })
+  expect(element).toBeDefined()
+  element = screen.getByText('Evil', { exact: false })
+  expect(element).toBeDefined()
+
+
 })
