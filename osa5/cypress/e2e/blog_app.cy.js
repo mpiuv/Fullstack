1b692965657ad1e077d4e0b065a346c77880cc56
T. L <43107946+mpiuv@users.noreply.github.com>
2022-12-08 14:40:55 +0200

    Väliversio tiedostojen siirtoa varten

--- osa5/cypress/e2e/blog_app.cy.js
+++ osa5/cypress/e2e/blog_app.cy.js
@@ -1,18 +1,32 @@
 describe('Blog ', function() {
-  beforeEach(function() {
+  beforeEach( function() {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+
+    const user = { username: 'root', user: 'Evil', password:'sekret' }
+    cy.request('POST','http://localhost:3003/api/users', user)
+
     cy.visit('http://localhost:3000')
   })
 
-  it('Login page is visible', function() {
+  it('Login form is shown', function() {
     cy.contains('Log in to application')
     cy.contains('username')
     cy.contains('password')
   })
 
-//  it('user can login', function () {
-//    cy.get('input:first').type('mluukkai')
-//    cy.get('input:last').type('salainen')
-//    cy.contains('login').click()
-//  })
+  describe('Login',function() {
+    it('succeeds with correct credentials', function () {
+      cy.get('input:first').type('root')
+      cy.get('input:last').type('sekret')
+      cy.contains('login').click()
+      cy.contains('root logged in')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.get('input:first').type('root1')
+      cy.get('input:last').type('sekret')
+      cy.contains('login').click()
+      cy.contains('Log in to application')
+    })
+  })
 })
