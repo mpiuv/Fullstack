1d5680842b9d15355bb25e914800a6b04a75fe52
T. L <43107946+mpiuv@users.noreply.github.com>
2023-02-16 13:23:39 +0200

    8.2

--- osa8/library-backend.js
+++ osa8/library-backend.js
@@ -98,18 +98,25 @@ let books = [
 */
 
 const typeDefs = `
-  type Query {
-    dummy: Int,
-    bookCount: Int,
-    authorCount:Int
-  }
-`
+type Book {
+  id: ID!,
+  title: String!,
+  published: Int!,
+  author: String!,
+  genres: [String!]!
+}
 
+type Query {
+  bookCount: Int!,
+  authorCount: Int!,
+  allBooks: [Book!]!
+}
+`
 const resolvers = {
   Query: {
-    dummy: () => 0,
     bookCount: () => books.length,
-    authorCount: () => authors.length
+    authorCount: () => authors.length,
+    allBooks: () => books
   }
 }
 
