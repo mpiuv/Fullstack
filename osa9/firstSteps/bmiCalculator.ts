b90de8894040d9f74ea69f4b2b91d2212c48be59
T. L <43107946+mpiuv@users.noreply.github.com>
2023-04-12 14:28:18 +0300

    9.1

---
+++ osa9/firstSteps/bmiCalculator.ts
@@ -0,0 +1,14 @@
+const calculateBmi = (height: number, weight: number) => {
+  height=height/100;
+  const bmi: number= weight/(height*height);
+  if(bmi<18.5)
+    return "Underweight"
+  else if (bmi<25)
+    return "Normal (healthy weight)"
+  else if (bmi<30)
+    return "Overweight"
+  else return "Obese";
+  //underweight (under 18.5 kg/m2), normal weight (18.5 to 24.9), overweight (25 to 29.9), and obese (30 or more)
+}
+
+console.log(calculateBmi(180, 74));
