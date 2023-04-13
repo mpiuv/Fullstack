f918fc08c9de71e0f03422882a95c12485ed491d
T. L <43107946+mpiuv@users.noreply.github.com>
2023-04-13 14:23:12 +0300

    9.6

--- osa9/firstSteps/bmiCalculator.ts
+++ osa9/firstSteps/bmiCalculator.ts
@@ -2,14 +2,14 @@ export const calculateBmi = (height: number, weight: number) => {
   height=height/100;
   const bmi: number= weight/(height*height);
   if(bmi<18.5)
-    return "Underweight"
+    return "Underweight";
   else if (bmi<25)
-    return "Normal (healthy weight)"
+    return "Normal (healthy weight)";
   else if (bmi<30)
-    return "Overweight"
+    return "Overweight";
   else return "Obese";
   //underweight (under 18.5 kg/m2), normal weight (18.5 to 24.9), overweight (25 to 29.9), and obese (30 or more)
-}
+};
 //const height: number = Number(process.argv[2]);
 //const weight: number = Number(process.argv[3]);
 
--- osa9/firstSteps/exerciseCalculator.ts
+++ osa9/firstSteps/exerciseCalculator.ts
@@ -8,13 +8,13 @@ interface Result { periodLength: number,
 }
    
 const calculateExercises = (daily_exercise_hours:number[],target_amount:number): Result => {
-    const periodLength: number=daily_exercise_hours.length
-    const trainingDays: number=daily_exercise_hours.length-daily_exercise_hours.filter(day=> day===0).length
-    const target: number=target_amount
+    const periodLength: number=daily_exercise_hours.length;
+    const trainingDays: number=daily_exercise_hours.length-daily_exercise_hours.filter(day=> day===0).length;
+    const target: number=target_amount;
     const average: number=daily_exercise_hours.reduce((a, b) => (a + b)) / daily_exercise_hours.length;
-    const success:boolean= average>target
-    const rating: number=average<1?1:average<2?2:3
-    const ratingDescription: string=rating===1?"Not so good":rating===2?"not too bad but could be better":"Excellent"
+    const success:boolean= average>target;
+    const rating: number=average<1?1:average<2?2:3;
+    const ratingDescription: string=rating===1?"Not so good":rating===2?"not too bad but could be better":"Excellent";
 
   return { periodLength,
     trainingDays,
@@ -23,13 +23,13 @@ const calculateExercises = (daily_exercise_hours:number[],target_amount:number):
     ratingDescription,
     target,
     average 
-  }
+  };
 
-}
-const _arguments:number= process.argv.length
-const target:number=Number(process.argv[2])
-let exerciseHours:number[]=[]
+};
+const _arguments:number= process.argv.length;
+const target=Number(process.argv[2]);
+let exerciseHours:number[]=[];
 for (let i = 3; i < _arguments; i++){
-  exerciseHours=exerciseHours.concat(Number(process.argv[i]))  
+  exerciseHours=exerciseHours.concat(Number(process.argv[i]));
 }
-console.log(JSON.stringify(calculateExercises(exerciseHours,target)))
+console.log(JSON.stringify(calculateExercises(exerciseHours,target)));
