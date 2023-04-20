aaec6e7c632ae2db074c07d77ca6d2059b92ad7d
T. L <43107946+mpiuv@users.noreply.github.com>
2023-04-20 13:16:30 +0300

    9.10

--- osa9/Patientor/server/src/index.js
+++ osa9/Patientor/server/src/index.js
@@ -4,6 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const diagnosis_1 = __importDefault(require("./routes/diagnosis"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 const PORT = 3001;
@@ -11,6 +12,7 @@ app.get('/api/ping', (_req, res) => {
     console.log('someone pinged here');
     res.send('pong');
 });
+app.use('/api/diagnoses', diagnosis_1.default);
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
--- osa9/Patientor/server/src/index.ts
+++ osa9/Patientor/server/src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import diagnosisRouter from './routes/diagnosis';
 const app = express();
 app.use(express.json());
 
@@ -8,6 +9,10 @@ app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('pong');
 });
+
+
+
+app.use('/api/diagnoses', diagnosisRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
