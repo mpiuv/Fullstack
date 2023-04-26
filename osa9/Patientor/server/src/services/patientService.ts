c6c4797da3407edb0845c75a0b397394d2e2d158
T. L <43107946+mpiuv@users.noreply.github.com>
2023-04-26 12:46:30 +0300

    9.12

--- osa9/Patientor/server/src/services/patientService.ts
+++ osa9/Patientor/server/src/services/patientService.ts
@@ -1,5 +1,6 @@
 import patientData from '../../data/patients';
 import { Patient,NonSensitivePatientEntry } from '../types';
+import { v1 as uuid } from 'uuid';
 
 const patients: Patient[] = patientData;
 
@@ -7,6 +8,15 @@ const getEntries = () :NonSensitivePatientEntry [] => {
   return patientData.map(({ id, name, dateOfBirth, gender, occupation})=>({ id, name, dateOfBirth, gender, occupation}));
 };
 
+const addPatient=(name:string, dateOfBirth:string, ssn:string, gender:string, occupation:string):Patient => {
+  const newPatientEntry = {
+    id: uuid(), name:name, dateOfBirth:dateOfBirth, ssn:ssn, gender:gender, occupation:occupation
+  };
+
+  patientData.push(newPatientEntry);
+  return newPatientEntry;
+}
+
 export default {
-  getEntries
+  getEntries, addPatient
 };
