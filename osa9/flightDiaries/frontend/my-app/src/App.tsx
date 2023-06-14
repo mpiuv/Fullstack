bbf3279dde620f3eb9465f601efa46868ad75de5
T. L <43107946+mpiuv@users.noreply.github.com>
2023-06-14 13:32:59 +0300

    9.18

--- osa9/flightDiaries/frontend/my-app/src/App.tsx
+++ osa9/flightDiaries/frontend/my-app/src/App.tsx
@@ -3,18 +3,26 @@ import './App.css';
 import { useState, useEffect } from 'react';
 import { NonSensitiveDiaryEntry } from './types';
 import axios from 'axios';
+import { stringify } from 'querystring';
 
-function App() {
+function App():JSX.Element{
   const [diaries, setDiaries] = useState<NonSensitiveDiaryEntry[]>([]);
   const [newDate, setNewDate] = useState<string>('');
   const [newVisibility, setNewVisibility] = useState<string>('');
   const [newWeather, setNewWeather] = useState<string>('');
   const [newComment, setNewComment] = useState<string>('');
+  const [errorMessage, setErrorMessage] = useState<string>('');
+
   useEffect(() => {
     axios.get<NonSensitiveDiaryEntry[]>('http://localhost:3001/api/diaries').then(response => {
       setDiaries(response.data)
     })
   }, []);
+
+  interface ValidationError {
+    message: string;
+    errors: Record<string, string[]>
+  }
 
   const addNewDiaryEntry = (event: React.SyntheticEvent) => {
     event.preventDefault()
@@ -22,8 +30,13 @@ function App() {
      { date: newDate, visibility: newVisibility, weather: newWeather, comment: newComment })
     .then(response => {
       setDiaries(diaries.concat({date:response.data.date,visibility:response.data.visibility,weather:response.data.weather, id:response.data.id} ))
-    }) 
-  }
+      setErrorMessage('')
+    })
+    .catch(error => { if (axios.isAxiosError<ValidationError, Record<string, unknown>>(error)) {
+      setErrorMessage(JSON.stringify(error.response?.data))
+   
+  }})
+}
 
   const handleDateChange = (event: React.SyntheticEvent) => {
     setNewDate((event.target as HTMLInputElement).value)
@@ -40,9 +53,26 @@ function App() {
     setNewComment((event.target as HTMLInputElement).value)
   }
 
+  type NotificationProps = {
+    message: string
+  }
+
+  const Notification = ({ message }:NotificationProps):JSX.Element|null => {
+    if (message === null) {
+      return null
+    }
+  
+    return (
+      <div className="error" style={{ color: 'red' }}>
+        {message}
+      </div>
+    )
+  }
+
   return (
     <div >
       <h1>Add new entry</h1>
+      <Notification  message={ errorMessage }/>
       <form onSubmit={addNewDiaryEntry}>
         date:<input value={newDate} onChange={handleDateChange}/> <br></br>
         visibility:<input value={newVisibility} onChange={handleVisibilityChange}/><br></br>
